0a1888e023b473f9e5e7cec948e3a32ab2c8491b
WayneSzchenTFS <szchen@tfs.ca>
2023-02-03 09:35:52 -0500

    Feb 3

---
+++ app/src/main/java/com/szchen/accesmembres/MainActivity.java
@@ -0,0 +1,21 @@
+package com.szchen.accesmembres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void entrer(View view) {
+        startActivity(new Intent(getApplication(), MemberLogin.class));
+        finish();
+    }
+}
